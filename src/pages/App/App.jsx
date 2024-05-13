9c71458f1a093c566662167d7037e6eab5e5aead
Molly Calhoun <mollyfcalhoun@gmail.com>
2024-05-13 13:32:41 -0700

    Update app navigation for HomePage and NavBar

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
 import NewRoutinePage from '../NewRoutinePage/NewRoutinePage';
 import RoutinesPage from '../RoutinesPage/RoutinesPage';
 import NavBar from '../../components/NavBar/NavBar';
-// import HomePage from '../HomePage';
+import HomePage from '../HomePage/HomePage';
 // import ExercisesListPage from '../ExercisesPage';
 // import NewExercisePage from '../NewExercisePage';
 // import MedicationsListPage from '../MedicationsPage';
@@ -17,24 +17,18 @@ export default function App() {
 
   return (
     <main className="App">
-      { user ?
-          <>
             <NavBar user={user} setUser={setUser} />
             <Routes>
-              {/* <Route path="/" element={<HomePage />} />
+              <Route path="/" element={<HomePage />} />
               <Route path="/auth" element={<AuthPage />} />
-              <Route path="/exercises" element={<ExercisesListPage />} />
+              {/* <Route path="/exercises" element={<ExercisesListPage />} />
               <Route path="/exercises/new" element={<NewExercisePage />} />
               <Route path="/medications" element={<MedicationsListPage />} />
-              <Route path="/medications/new" element={<NewMedicationPage />} />
-              <Route path="/*" element={<Navigate to="/" />} /> */}
+              <Route path="/medications/new" element={<NewMedicationPage />} /> */}
               <Route path="/routines/new" element={<NewRoutinePage />} />
               <Route path="/routines" element={<RoutinesPage />} />
+              <Route path="/*" element={<Navigate to="/" />} />
             </Routes>
-          </>
-          :
-          <AuthPage setUser={setUser} />
-      }
     </main>
   );
 }
