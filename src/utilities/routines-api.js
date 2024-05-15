2e050631f48d48ccabb6e966d1183c71ee87e5ec
Molly Calhoun <mollyfcalhoun@gmail.com>
2024-05-15 12:20:42 -0700

    Deploy

--- src/utilities/routines-api.js
+++ src/utilities/routines-api.js
@@ -1,6 +1,10 @@
 import sendRequest from "./send-request";
 const BASE_URL = '/api/routines';
 
+export function getAllRoutines(routineData) {
+  return sendRequest(BASE_URL, 'POST', routineData);
+}
+
 export function addRoutine(routineData) {
   return sendRequest(BASE_URL, 'POST', routineData);
 }
